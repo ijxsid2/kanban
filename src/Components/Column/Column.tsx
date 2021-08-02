dd97ce9ac40b36bf61607fe805f7596d79695517
Inderjit Singh <ijxsid@gmail.com>
2021-08-02 21:10:54 +0530

    Add double click to edit functionality

--- src/Components/Column/Column.tsx
+++ src/Components/Column/Column.tsx
@@ -9,28 +9,42 @@ type Props = {
     addTask: () => void;
 }
 
-let Column = ({ column, columnTasks, addTask }: Props) => (
-    <div className="Column" key={column.name}>
-        <h3 className="Column__name">
-            {column.name}
-        </h3>
+let Column = ({ column, columnTasks, addTask }: Props) => {
+    const [isNameEditable, setIsNameEditable] = React.useState<boolean>(false)
 
-        <div className="Column__add-task">
-            <button className="btn" onClick={addTask}>
-                +
-            </button>
-        </div>
-        <ul className="Column__contents">
-            {
-                columnTasks.map(
-                    (task: TaskType) => (
-                        <Task task={task} key={task.id}/>
-                    )
-                )
+    const onDoubleClick = () => {
+        setIsNameEditable(true)
+    }
+
+    const onBlur = () => {
+        setIsNameEditable(false);
+    }
+    return (
+        <div className="Column">
+            { isNameEditable
+              ? <textarea rows={1} onBlur={onBlur}>{column.name}</textarea>
+              : <h3 className="Column__name" onDoubleClick={onDoubleClick}>
+                {column.name}
+                </h3> 
             }
-        </ul>
-    </div>
-            
-)
 
+
+            <div className="Column__add-task">
+                <button className="btn" onClick={addTask}>
+                    +
+                </button>
+            </div>
+            <ul className="Column__contents">
+                {
+                    columnTasks.map(
+                        (task: TaskType) => (
+                            <Task task={task} key={task.id}/>
+                        )
+                    )
+                }
+            </ul>
+        </div>
+            
+    )
+}
 export default Column;
--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -6,12 +6,43 @@ type Props = {
     task: TaskType
 }
 
-let Task = ({ task }: Props) => (
-    <li className="Column__item card" key={task.id}>
-        <div className="card-header">
-            {task.title}
-        </div>
-    </li>
-)
+let Task = ({ task }: Props) => {
+    const [isEditable, setIsEditable] = React.useState<boolean>(false)
 
+    const onDoubleClick = () => {
+        setIsEditable(true)
+    }
+
+    const onBlur = () => {
+        setIsEditable(false);
+    }
+    return (
+        <li className="Column__item card"
+            onDoubleClick={onDoubleClick}
+            >
+            {
+                isEditable ? 
+                <textarea onBlur={onBlur}>{task.title}</textarea> :
+                <div className="card-header Task__title">
+                    {task.title}
+                </div>
+            }
+            <div className="card-footer Task__actions">
+                <div className="left">
+                    <button className="btn">
+                        <i className="icon icon-arrow-left"/>
+                    </button>
+                    <button className="btn btn-error Task__delete">
+                        <i className="icon icon-delete"/>
+                    </button>
+                </div>
+ 
+                <div className="btn right">
+                    <i className="icon icon-arrow-right"/>
+                </div>
+            </div>
+
+        </li>
+    )
+}
 export default Task;
